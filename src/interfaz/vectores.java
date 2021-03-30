066c1c23e34e5d750c446b1e6f48123660972947
cristian1920 <cristian131411@gmail.com>
2021-03-30 14:12:22 -0500

    importar proyecto

---
+++ src/interfaz/vectores.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+/**
+ *
+ * @author casa
+ */
+public class vectores {
+     
+    public static String[] codigo={"001","002","003","004","005"};
+    public static String[] articulos={"Leche","Pan","Queso","Huevo","Arepa"};
+    public static int[] preciocompra={1200,800,1600,200,1500};
+    public static int[] cantidadbodega={5,5,5,5,5};
+   
+}
+
